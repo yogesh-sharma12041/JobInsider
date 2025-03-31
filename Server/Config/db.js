e2b8b65b411c3e9643c10447a797a4c74289c894
Yogesh Sharma <yogesh.sharma12041@gmail.com>
2025-03-31 18:10:07 +0530

    Done Changes

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 //Function to connect to the MongoDB Database
 
-const connectMongoDB = async() => {
-    mongoose.connection.on('connected', () => console.log("Database is connected")
-    )
+const connectMongoDB = async () => {
+  mongoose.connection.on("connected", () =>
+    console.log("Database is connected")
+  );
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
-}
+  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+};
 
 export default connectMongoDB;
