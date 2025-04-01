2152d53dbb18da5f00e8808700e83270aefc3b35
Yogesh Sharma <yogesh.sharma12041@gmail.com>
2025-04-01 09:46:21 +0530

    Fix Bug

--- Server/Models/User-Model.js
+++ Server/Models/User-Model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id: {type: String, required},
+    _id: {type: String, required: true},
     name: {type: String, required: true},
     email: {type:String, required: true, unique: true},
     resume: {type: String},
